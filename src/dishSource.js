7b652ed013e8a42d70d00ab557770b03ffd12ab9
litian <litian@kth.se>
2023-02-12 13:36:45 +0100

    2.3 finished

--- src/dishSource.js
+++ src/dishSource.js
@@ -10,15 +10,37 @@ function responseHandlerACB(response){
     return response.json();
 }
 
-function processDataACB(data){
-    return data;
+function getDishDetails(id){
+    return getMenuDetails([id]).then(resultProcessACB);
+}   
+
+function resultProcessACB(data){
+    return data[0];
+}
+function searchDishes(object){
+    return fetch(BASE_URL + "recipes/complexSearch?" + 
+    new URLSearchParams({query:object.query, type:object.type}), 
+    {method: 'GET', headers:{'X-Mashape-Key': API_KEY}}).then(responseHandlerACB).then(dataHandlerACB);
 }
 
-function arrayCommaACB(array){
-    if(!array.ok) throw new Error("API problem "+array.status);
-    return array.join();
+function searchConditions(object){
+
+    if(object.query && object.type){
+        return new URLSearchParams({query:object.query, type:object.type});
+    }
+    else if (!object.query && object.type){
+        return new URLSearchParams({type:object.type});
+    }
+    else if (object.query && !object.type)
+    return new URLSearchParams({type:object.type});
+
+
+
 }
 
 
-    
-export{getMenuDetails};
+function dataHandlerACB(data){
+    return data.results;
+}
+
+export{getMenuDetails, searchDishes, getDishDetails};
