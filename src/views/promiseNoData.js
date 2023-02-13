157e8aafd77da4cb0748545edd248c71a84daaa3
Raymond Chen <rayche@kth.se>
2023-02-13 16:32:27 +0100

    2nd fix to loading image rerorero

--- src/views/promiseNoData.js
+++ src/views/promiseNoData.js
@@ -2,7 +2,7 @@ export default function promiseNoData(promState){
     if (!promState.promise)
         return <div>No data</div>;
     if (promState.promise && !promState.data && !promState.error)
-        return <img src="https://media.tenor.com/9PHuBQSONZcAAAAC/jojos-bizarre-adventures-rero-rero.gif" class="rerorero"/>;
+        return <img src="https://media.tenor.com/9PHuBQSONZcAAAAC/jojos-bizarre-adventures-rero-rero.gif" class = "rerorero"/>;
     if (promState.promise && !promState.data && promState.error)
         return <div>{promState.error.toString()}</div>;
     if (promState.promise && promState.data && !promState.error)
--- src/vuejs/detailsPresenter.js
+++ src/vuejs/detailsPresenter.js
@@ -2,12 +2,12 @@ import DetailsView from "../views/detailsView.js";
 import promiseNoDate from "../views/promiseNoData.js";
 export default
 function Detail(aa){
-    return <div> {promiseNoDate(aa.model.currentDishPromiseState) ||
+    return promiseNoDate(aa.model.currentDishPromiseState)||
         <DetailsView dishData={aa.model.currentDishPromiseState.data} 
         isDishInMenu = {aa.model.dishes.filter(isDishInMenuHandlerACB).length === 1}
         guests = {aa.model.numberOfGuests}
         addToMenu = {addToMenuHandlerACB}
-    />}</div>;
+    />;
 
     function isDishInMenuHandlerACB(dish){
         return (aa.model.currentDish === dish.id);
