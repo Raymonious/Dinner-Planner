4851753a423b02b26735db6e3cb240292a11e484
Raymond Chen <rayche@kth.se>
2023-03-01 11:21:02 +0100

    TW3.2 DetailPresenter done
    Co-authored-by: Litian Lei <litian@kth.se>

--- src/vuejs/detailsPresenter.js
+++ src/vuejs/detailsPresenter.js
@@ -1,19 +1,44 @@
 import DetailsView from "../views/detailsView.js";
-import promiseNoDate from "../views/promiseNoData.js";
-export default
-function Detail(aa){
-    return promiseNoDate(aa.model.currentDishPromiseState)||
-        <DetailsView dishData={aa.model.currentDishPromiseState.data} 
-        isDishInMenu = {aa.model.dishes.filter(isDishInMenuHandlerACB).length === 1}
-        guests = {aa.model.numberOfGuests}
-        addToMenu = {addToMenuHandlerACB}
-    />;
+import promiseNoData from "../views/promiseNoData.js";
+import {watchEffect,reactive} from "vue"; 
+import resolvePromise from "../resolvePromise.js"
+import { getDishDetails } from "../dishSource.js";
 
-    function isDishInMenuHandlerACB(dish){
-        return (aa.model.currentDish === dish.id);
-    }
+//TW3 Search, Details, ...
+export default{
+    name: "DetailsPresenter",   // useful for Vue stacktraces
+    props:["model"],
+    
+    setup(aa){
+        const promiseState= reactive({});
+       
+        function isDishInMenuHandlerACB(dish){
+            return (aa.model.currentDish === dish.id);
+        }
 
-    function addToMenuHandlerACB(){
-        aa.model.addToMenu(aa.model.currentDishPromiseState.data);
-    }
-}
+        function addToMenuHandlerACB(){
+            aa.model.addToMenu(promiseState.data);
+        }
+
+        function hasChangedACB(){
+            console.log("current dish changes to: ", aa.model.currentDish);
+            // good place to resolve current dish promise in component state
+            // you can also cancel the effect / promise,  not needed in the lab: 
+            if (aa.model.currentDish)
+            resolvePromise(getDishDetails(aa.model.currentDish), promiseState); 
+            
+        }
+        watchEffect(hasChangedACB);
+
+        return function renderACB(){return promiseNoData(promiseState)||
+            <DetailsView dishData={promiseState.data} 
+            isDishInMenu = {aa.model.dishes.filter(isDishInMenuHandlerACB).length === 1}
+            guests = {aa.model.numberOfGuests}
+            addToMenu = {addToMenuHandlerACB}
+        />;};
+    },
+};
+
+
+
+
