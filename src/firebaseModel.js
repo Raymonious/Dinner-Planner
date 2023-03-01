1f0a8d037ca747d5a29d9968fd1666d817c0fadf
Raymond Chen <rayche@kth.se>
2023-03-01 17:33:11 +0100

    TW3.5 firebase unit test passed

--- src/firebaseModel.js
+++ src/firebaseModel.js
@@ -1,6 +1,7 @@
 const { initializeApp }= require( "firebase/app");
 const { getDatabase, ref, get, set, onValue}= require( "/src/teacherFirebase.js");
 
+import DinnerModel from "./DinnerModel";
 import { getMenuDetails } from "./dishSource";
 import resolvePromise from "./resolvePromise";
 // this is needed so that unit tests can inject a mock firebase
@@ -20,16 +21,26 @@ const db= getDatabase(app)
 
 //root firebase path of group 06
 const PATH="dinnerModel06";
-const rf= ref(db, PATH+"/test");
-set(rf, "dummy"); 
+const rf= ref(db, PATH+"/test4");
+const aModel= new DinnerModel(5, [{id:42}, {id:13}]); 
+aModel.setCurrentDish(13);
+set(rf, modelToPersistence(aModel));
 
-function observerRecap(/*TODO*/) {
-    //TODO
+
+
+function observerRecap(model){
+    function obsACB(payload) {
+        console.log(payload);
+    }
+    model.addObserver(obsACB);
 }
+
 
 function modelToPersistence(model){
     // TODO return an object
-    return model.dishes.map(getIDCB).sort();
+    let curDish = model.currentDish;
+    if(!model.currentDish) curDish = null;
+    return {numberOfGuests: model.numberOfGuests, dishes: model.dishes.map(getIDCB).sort(), currentDish: curDish};
     function getIDCB(dish){
         return dish.id;
     }
@@ -38,17 +49,22 @@ function modelToPersistence(model){
 
 function persistenceToModel(persistedData, model){
     // TODO return a promise
-    model.setNumberOfGuests(persistedData.numberOfGuests);
-    if(!persistedData[currentDish])
-    model.setCurrentDish(persistedData[currentDish]);
-
-    return getMenuDetails(persistedData.dishes).then(modelChangerACB);
-    function modelChangerACB(dishes){
-        model.dishes = dishes;
-        return model;
+    if(!persistedData){
+        model.setNumberOfGuests(2);
+        model.dishes = []
     }
+    else{
+        model.setNumberOfGuests(persistedData.numberOfGuests);
+        model.setCurrentDish(persistedData.currentDish);
+        getMenuDetails(persistedData.dishes).then(updateDishACB);
 
+        function updateDishACB(dishes){
+            model.dishes = dishes;
+        }
 
+    }
+    return model;
+    
 }
 
 
@@ -57,11 +73,28 @@ function persistenceToModel(persistedData, model){
 function firebaseModelPromise(model) {
     // TODO return a promise chain that
     // 1) retrieves data from firebase using firebase get()
+    return get(rf).then(dataHandlerACB).then(wakeObserverACB);
     // 2) saves the data into the model (received as parameter)
+    function dataHandlerACB(dataFromFirebase){
+        return persistenceToModel(dataFromFirebase.val(), model);
+    }
     // 3) adds a model observer that calls firebase set() and modelToPersistence()
+    //add observer after the model has been processed by persistenceToModel
+    function wakeObserverACB(model){
+        model.addObserver(obsACB); 
+        return model;
+    }
+
+    function obsACB() {
+        //TODO
+        set(rf,modelToPersistence(model));
+    }
+    
+
+
     // 4) optional: calls firebase onValue() for live update
 }
 
 
 // Remember to uncomment the following line:
-export {observerRecap, /*modelToPersistence, persistenceToModel, firebaseModelPromise*/};
+export {observerRecap, modelToPersistence, persistenceToModel, firebaseModelPromise};
