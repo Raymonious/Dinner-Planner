413b7b41cb8854d9c4873cacd26b86189a3a0f07
Raymond Chen <rayche@kth.se>
2023-03-01 11:24:18 +0100

    TW3.5 battling firebaseModel

--- src/firebaseModel.js
+++ src/firebaseModel.js
@@ -1,23 +1,58 @@
 const { initializeApp }= require( "firebase/app");
 const { getDatabase, ref, get, set, onValue}= require( "/src/teacherFirebase.js");
 
+import { getMenuDetails } from "./dishSource";
+import resolvePromise from "./resolvePromise";
+// this is needed so that unit tests can inject a mock firebase
+import firebaseConfig from "/src/firebaseConfig.js";
+// other imports are already provided for you! 
+
+
+
+
 // Add relevant imports here 
 // TODO
 
 // Initialise firebase app, database, ref
 // TODO
+const app= initializeApp(firebaseConfig)
+const db= getDatabase(app)
+
+//root firebase path of group 06
+const PATH="dinnerModel06";
+const rf= ref(db, PATH+"/test");
+set(rf, "dummy"); 
 
 function observerRecap(/*TODO*/) {
     //TODO
 }
 
-function modelToPersistence(/* TODO */){
+function modelToPersistence(model){
     // TODO return an object
+    return model.dishes.map(getIDCB).sort();
+    function getIDCB(dish){
+        return dish.id;
+    }
+
 }
 
-function persistenceToModel(/* TODO */){
+function persistenceToModel(persistedData, model){
     // TODO return a promise
+    model.setNumberOfGuests(persistedData.numberOfGuests);
+    if(!persistedData[currentDish])
+    model.setCurrentDish(persistedData[currentDish]);
+
+    return getMenuDetails(persistedData.dishes).then(modelChangerACB);
+    function modelChangerACB(dishes){
+        model.dishes = dishes;
+        return model;
+    }
+
+
 }
+
+
+
 
 function firebaseModelPromise(model) {
     // TODO return a promise chain that
@@ -29,4 +64,4 @@ function firebaseModelPromise(model) {
 
 
 // Remember to uncomment the following line:
-// export {observerRecap, modelToPersistence, persistenceToModel, firebaseModelPromise};
+export {observerRecap, /*modelToPersistence, persistenceToModel, firebaseModelPromise*/};
